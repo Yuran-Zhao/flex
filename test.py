341490708d3b6f967882a78b637a1292be18408a
lovexxf <zyr1998527@126.com>
2018-11-04 16:11:57 +0800

    检验文件

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+      
+def  main():
+    a = 1e10
+    b = 5E-2
+    c = 123+456
+    d = 3.4e3-5E-2
+    e = 34+8E2+7.3e-1-789
+    print a,b,c,d,e
+
+if   __name__  ==  '__main__':
+    main()  
+
+
