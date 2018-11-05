7daa3829aca20014b8d542f03ee2daa2bff99e27
zyr <zyr1998527@126.com>
2018-11-05 17:05:46 +0800

    version 1.0

---
+++ result.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+      
+def  main():
+    a = 10000000000.000
+    b = 0.050
+    c = 579.000
+    d = 3399.950
+    e = 45.730
+    print a,b,c,d,e
+
+if   __name__  ==  '__main__':
+    main()  
+
+
